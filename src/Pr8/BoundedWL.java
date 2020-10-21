0b09ab0dab32a7eccbb517a35daaf5f60af704b4
Pavel Chernyaev <p.tchernyaev@yandex.ru>
2020-10-21 14:04:29 +0300

    Initial commit

---
+++ src/Pr8/BoundedWL.java
@@ -0,0 +1,28 @@
+package Pr8;
+
+public class BoundedWL<E> extends WaitList<E>{
+
+    private int capacity;
+
+    public BoundedWL(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void add(E element) {
+
+        if(this.content.size() < this.capacity) {
+            super.add(element);
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return this.content.toString() + ". Capacity " + this.capacity;
+    }
+}
+
